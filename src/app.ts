88c87d57ec490d15971dfb72ccb67d6ca2ab5c44
dafu1985 <tianshandafu894@gmail.com>
2026-03-09 15:10:07 +0900

    feat: add CORS settings and frontend integration docs

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,22 @@
 import express from "express";
+import cors from "cors";
 import userRouter from "./modules/user/user.routes";
+import authRouter from "./modules/auth/auth.routes";
 import { errorHandler } from "./middlewares/error.middleware";
-import authRouter from "./modules/auth/auth.routes"; 
 import { notFoundHandler } from "./middlewares/notFound.middleware";
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
+
 app.use(express.json());
 
 app.use("/users", userRouter);
-app.use("/auth", authRouter); 
+app.use("/auth", authRouter);
 
 // 404
 app.use(notFoundHandler);
@@ -22,4 +28,4 @@ app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
 
-export default app; 
+export default app;
